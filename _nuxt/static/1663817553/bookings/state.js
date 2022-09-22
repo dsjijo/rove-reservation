eef6887dcc92640f2748e70739d9e0855f0dfa58
Jijo Joseph <jijo.joseph@datasemantics.co>
2022-09-22 10:53:19 +0400

    updates

---
+++ _nuxt/static/1663817553/bookings/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663817553",layout:"default",error:a,state:{counter:0,loader:false,mails:[],mail:a,bookings:[]},serverRendered:true,routePath:"\u002Fbookings",config:{app_version:"1.0.0",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1663817553/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663817553",layout:"default",error:a,state:{counter:0,loader:false,mails:[],mail:a,bookings:[]},serverRendered:true,routePath:b,config:{app_version:"1.0.0",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
